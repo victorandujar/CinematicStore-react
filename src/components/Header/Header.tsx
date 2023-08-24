14c91619ac1e678a0443a2ac7b0e7de374cde80e
victorandujar <victorandujarmurcia@gmail.com>
2023-08-24 17:05:39 +0200

    Create Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import HeaderStyled from "./HeaderStyled";
+
+const Header = (): React.ReactElement => {
+  return (
+    <HeaderStyled className="header">
+      <h1 className="header__title">Cinematic Store</h1>
+      <img
+        src="../../../public/logo.png"
+        alt="Cinematic Store logo"
+        width={50}
+        height={50}
+        className="header__logo"
+      />
+    </HeaderStyled>
+  );
+};
+
+export default Header;
