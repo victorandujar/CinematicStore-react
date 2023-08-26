eca99f8817221956e1a7c3c7ff8ded9b194ab1aa
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-08-26 22:31:11 +0200

    Change tests because of adding renderProviders at testUtils

--- src/components/MovieCard/MovieCard.test.tsx
+++ src/components/MovieCard/MovieCard.test.tsx
@@ -1,20 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { movieMockAvatar } from "../../mocks/movieMocks";
 import MovieCard from "./MovieCard";
 import "@testing-library/jest-dom";
-import GlobalStyles from "../../styles/GlobalStyles";
+import { renderWithProviders } from "../../utils/testUtils/renderProviders";
 
 describe("Given a MovileCard component", () => {
   describe("When it is rendered", () => {
     test("Then it should show a title with the text 'Avatar: El sentido del agua", () => {
       const cardTitle = movieMockAvatar.title;
 
-      render(
-        <>
-          <GlobalStyles />
-          <MovieCard movie={movieMockAvatar} />
-        </>,
-      );
+      renderWithProviders(<MovieCard movie={movieMockAvatar} />);
 
       const expectedHeader = screen.getByRole("heading", { name: cardTitle });
 
--- src/components/MovieCardList/MovieCardList.test.tsx
+++ src/components/MovieCardList/MovieCardList.test.tsx
@@ -1,15 +1,18 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import MovieCardList from "./MovieCardList";
 import { moviesMock } from "../../mocks/movieMocks";
 import "@testing-library/jest-dom";
 import { MoviesDataStructure } from "../../types/movie";
+import { renderWithProviders } from "../../utils/testUtils/renderProviders";
 
 describe("Given a MovieCardList component", () => {
   describe("When it is rendered", () => {
     test("Then it should show a card with a header text'Avatar: El sentido del agua'", () => {
       const headerText = "Avatar: El sentido del agua";
 
-      render(<MovieCardList movies={moviesMock as MoviesDataStructure} />);
+      renderWithProviders(
+        <MovieCardList movies={moviesMock as MoviesDataStructure} />,
+      );
 
       const expectedHeader = screen.getByRole("heading", { name: headerText });
 
